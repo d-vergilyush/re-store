b24e1649d7add306751319e2deabdb0fc663320d
Dana Vergilyush <dvergilyush@gmail.com>
2020-08-16 17:26:04 +0300

    Create `BookList`

---
+++ src/components/book-list/book-list.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../../actions'
+
+import BookListItem from '../book-list-item';
+
+import './book-list.css';
+
+class BookList extends Component {
+  render() {
+    const { books } = this.props;
+
+    return (
+      <ul>
+        {
+          books.map((book) => {
+            return (
+              <li key={book.id}><BookListItem book={book}/></li>
+            )
+          })
+        }
+      </ul>
+    );
+  }
+}
+
+const mapStateToProps = ({ books }) => {
+  return { books }; 
+};
+
+export default connect(mapStateToProps, actions)(BookList);
