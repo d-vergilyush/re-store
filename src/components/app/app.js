b2b1118f94b4707cd28f2469b0a06cd49e04f9a9
Dana Vergilyush <dvergilyush@gmail.com>
2020-08-16 15:42:17 +0300

    Create `App`

---
+++ src/components/app/app.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import { HomePage, CartPage } from '../pages';
+
+import './app.css';
+
+const App = () => {
+  return (
+    <Switch>
+      <Route 
+        path="/"
+        component={HomePage}
+        exact
+      />
+      <Route 
+        path="/cart"
+        component={CartPage}
+      />
+      <Route render={() => <h2>404</h2> } />
+    </Switch>
+  );
+};
+
+export default App;
