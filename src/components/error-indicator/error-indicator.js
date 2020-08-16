cb0dd13a5ff020356b4958769b2ae707b87d806b
Dana Vergilyush <dvergilyush@gmail.com>
2020-08-16 15:40:03 +0300

    Create `ErrorIndicator`

---
+++ src/components/error-indicator/error-indicator.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorIndicator = () => {
+  return (
+    <div className="error-indicator">
+      <span className="msg">Error!</span>
+    </div>
+  );
+};
+
+export default ErrorIndicator;
