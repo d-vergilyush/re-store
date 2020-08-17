0cd12245393e022b89d5c1c8dd6b0ce29c94184e
Dana Vergilyush <dvergilyush@gmail.com>
2020-08-17 13:56:14 +0300

    Edit `ShopHeader`

--- src/components/shop-header/shop-header.js
+++ src/components/shop-header/shop-header.js
@@ -1,15 +1,18 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './shop-header.css';
 
 const ShopHeader = ({ numItems, total }) => {
   return (
     <header className="shop-header row">
-      <a className="logo text-dark" href="#">ReStore</a>
-      <a className="shopping-cart">
+      <Link to="./">
+        <div className="logo text-dark" href="#">ReStore</div>
+      </Link>
+      <Link to="./cart" className="shopping-cart">
         <i className="cart-icon fa fa-shopping-cart" />
         {numItems} items (${total})
-      </a>
+      </Link>
     </header>
   );
 };
