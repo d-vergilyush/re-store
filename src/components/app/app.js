5ac556cc25deed38516eda6414ebf478218b99a3
Dana Vergilyush <dvergilyush@gmail.com>
2020-08-17 04:33:03 +0300

    Improve `App`

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import ShopHeader from '../shop-header';
 import { HomePage, CartPage } from '../pages';
 
 import './app.css';
 
 const App = () => {
   return (
-    <Switch>
-      <Route 
-        path="/"
-        component={HomePage}
-        exact
-      />
-      <Route 
-        path="/cart"
-        component={CartPage}
-      />
-      <Route render={() => <h2>404</h2> } />
-    </Switch>
+    <main role="main" className="container">
+      <ShopHeader numItems={5} total={210} />
+      <Switch>
+        <Route 
+          path="/"
+          component={HomePage}
+          exact />
+        <Route 
+          path="/cart"
+          component={CartPage} />
+        <Route render={() => <h2>404</h2> } />
+      </Switch>
+    </main>
   );
 };
 
