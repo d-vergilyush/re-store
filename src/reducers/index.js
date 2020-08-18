07eb03074b3c6020b575cd6e9d26db0721cc810d
Dana Vergilyush <dvergilyush@gmail.com>
2020-08-18 18:59:21 +0300

    Update `reducer`. Add new items to `initialState`

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,25 +1,30 @@
 const initialState = {
   books: [],
   loading: true,
-  error: null
+  error: null,
+  cartItems: [],
+  orderTotal: 0
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case "FETCH_BOOKS_SUCCESS":
       return {
+        ...state,
         books: [],
         loading: true,
         error: null
       };
     case "FETCH_BOOKS_REQUEST":
       return {
+        ...state,
         books: action.payload,
         loading: false,
         error: null
       };
     case "FETCH_BOOKS_FAILURE":
       return {
+        ...state,
         books: [],
         loading: false,
         error: action.payload
