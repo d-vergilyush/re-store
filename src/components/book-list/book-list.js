4173ebb3ef45dd75edc8693c2eabc5377bdb5ab4
Dana Vergilyush <dvergilyush@gmail.com>
2020-08-18 17:22:22 +0300

    Destructure `BookList

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -10,7 +10,18 @@ import { compose } from "../../utils";
 
 import "./book-list.css";
 
-class BookList extends Component {
+const BookList = ({ books }) => {
+  return (
+    <ul className="book-list">
+      {books.map((book) => (
+        <li key={book.id}>
+          <BookListItem book={book} />
+        </li>
+      ))}
+    </ul>
+  );
+};
+class BookListContainer extends Component {
   componentDidMount() {
     this.props.fetchBooks();
   }
@@ -26,15 +37,7 @@ class BookList extends Component {
       return <ErrorIndicator />;
     }
 
-    return (
-      <ul className="book-list">
-        {books.map((book) => (
-          <li key={book.id}>
-            <BookListItem book={book} />
-          </li>
-        ))}
-      </ul>
-    );
+    return <BookList books={books} />;
   }
 }
 
@@ -51,4 +54,4 @@ const mapDispatchToProps = (dispatch, { bookstoreService }) => ({
 export default compose(
   withBookstoreService(),
   connect(mapStateToProps, mapDispatchToProps)
-)(BookList);
+)(BookListContainer);
