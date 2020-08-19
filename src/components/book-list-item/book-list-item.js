227f539b69b286de8ffabf10bfc88072c6be38ca
Dana Vergilyush <dvergilyush@gmail.com>
2020-08-19 04:18:03 +0300

    Improve `BookListItem`

--- src/components/book-list-item/book-list-item.js
+++ src/components/book-list-item/book-list-item.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 import './book-list-item.css';
 
-const BookListItem = ({ book }) => {
+const BookListItem = ({ book, onAddedToCart }) => {
   const { title, author, price, coverImage } = book;
   return (
     <div className="book-list-item">
@@ -11,12 +11,18 @@ const BookListItem = ({ book }) => {
         <img src={coverImage} alt="cover" />
       </div>
       <div className="book-details">
-        <a href="#" className="book-title">{title}</a>
+        <a href="#" className="book-title">
+          {title}
+        </a>
         <div className="book-author">{author}</div>
         <div className="book-price">${price}</div>
-        <button className="btn btn-info add-to-cart">Add to cart</button>
+        <button
+          onClick={onAddedToCart}
+          className="btn btn-info add-to-cart"
+        >
+          Add to cart
+        </button>
       </div>
-
     </div>
   );
 };
