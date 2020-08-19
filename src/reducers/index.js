38680147e106de53ccd1ea5060fd8af89ba87578
Dana Vergilyush <dvergilyush@gmail.com>
2020-08-19 04:18:21 +0300

    Update reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,6 +6,37 @@ const initialState = {
   orderTotal: 0
 };
 
+const updateCartItems = (cartItems, item, idx) => {
+  if (idx === -1) {
+    return [
+      ...cartItems,
+      item
+    ];
+  }
+
+  return [
+    ...cartItems.slice(0, idx),
+    item,
+    ...cartItems.slice(idx + 1)
+  ];
+};
+
+const updateCartItem = (book, item = {}) => {
+
+  const {
+    id = book.id,
+    quantity = 0,
+    title = book.title,
+    total = 0 } = item;
+
+  return {
+    id,
+    title,
+    quantity: quantity + 1,
+    total: total + book.price
+  };
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case "FETCH_BOOKS_SUCCESS":
@@ -29,6 +60,19 @@ const reducer = (state = initialState, action) => {
         loading: false,
         error: action.payload
       };
+    case "BOOKS_ADDED_TO_CART":
+      const { cartItems, books } = state;
+      const bookId = action.payload;
+      const book = books.find(({id}) => id === bookId);
+      const itemIndex = cartItems.findIndex(({id}) => id === bookId);
+      const item = cartItems[itemIndex];
+      const newItem = updateCartItem(book, item);
+      
+      return {
+        ...state,
+        cartItems: updateCartItems(cartItems, newItem, itemIndex)
+      };
+
     default:
       return state;
   }
