7d873d34aa7dc0411ed9dec732a4e9eafe1836f8
Dana Vergilyush <dvergilyush@gmail.com>
2020-08-20 23:29:09 +0300

    Update action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -25,7 +25,23 @@ const bookAddedToCart = (bookId) => ({
   payload: bookId
 });
 
+const bookRemovedFromCart = (bookId) => {
+  return {
+    type: 'BOOK_REMOVED_FROM_CART',
+    payload: bookId
+  };
+};
+
+const allBooksRemovedFromCart = (bookId) => {
+  return {
+    type: 'ALL_BOOKS_REMOVED_FROM_CART',
+    payload: bookId
+  };
+};
+
 export {
   fetchBooks,
-  bookAddedToCart
+  bookAddedToCart,
+  bookRemovedFromCart,
+  allBooksRemovedFromCart
 };
