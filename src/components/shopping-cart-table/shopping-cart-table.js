4532811efec88f4c8b1ec7d9f95118044ac34b63
Dana Vergilyush <dvergilyush@gmail.com>
2020-08-21 18:32:30 +0300

    Fix `ShoppingCartTable`

--- src/components/shopping-cart-table/shopping-cart-table.js
+++ src/components/shopping-cart-table/shopping-cart-table.js
@@ -72,7 +72,7 @@ const ShoppingCartTable = ({
   );
 };
 
-const mapStateToProps = ({ cartItems, orderTotal }) => ({
+const mapStateToProps = ({ shoppingCart: { cartItems, orderTotal } }) => ({
   items: cartItems,
   total: orderTotal
 });
