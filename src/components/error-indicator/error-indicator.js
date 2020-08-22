7eb678e5cb5105fdc9fbf5b7f91508ab9945c497
Dana Vergilyush <dvergilyush@gmail.com>
2020-08-22 23:24:39 +0300

    Update `ErrorIndicator`

--- src/components/error-indicator/error-indicator.js
+++ src/components/error-indicator/error-indicator.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './error-indicator.css';
 
 const ErrorIndicator = () => {
   return (
     <div className="error-indicator">
-      <span className="msg">Error!</span>
+      <span className="msg">Oops! Something went wrong!</span>
     </div>
   );
 };
